bea3b1cd688e575c85b84a8d750d8c57034ec1ce
Oshioke-Salaki <salaki1902@gmail.com>
2023-09-05 14:02:23 -0600

    first commit

---
+++ src/ui/Header.jsx
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom';
+import SearchOrder from '../features/order/SearchOrder';
+function Header() {
+  return (
+    <header>
+      <Link to="/">Fast Pizza Co.</Link>
+      <SearchOrder />
+      <p>Oshioke</p>
+    </header>
+  );
+}
+
+export default Header;
