8cabf1ca7fc005a12da804649cb2bb3e8604f757
Oshioke-Salaki <salaki1902@gmail.com>
2023-09-06 12:32:46 -0600

    added tailwind

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -1,12 +1,12 @@
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom';
 function CartOverview() {
   return (
-    <div>
-      <p>
+    <div className="bg-stone-800 text-stone-200 uppercase">
+      <p className="text-stone-300 font-semibold">
         <span>23 pizzas</span>
         <span>$23.45</span>
       </p>
-      <Link to='/cart'>Open cart &rarr;</Link>
+      <Link to="/cart">Open cart &rarr;</Link>
     </div>
   );
 }
---
+++ src/features/user/Username.jsx
@@ -0,0 +1,5 @@
+function Username() {
+  return <p className="text-sm font-semibold">Oshioke Salaki</p>;
+}
+
+export default Username;
