89b0699b5034395b1f64ddb64e0b17ad633703f2
Oshioke-Salaki <salaki1902@gmail.com>
2023-09-12 11:47:13 -0600

    update

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -2,35 +2,13 @@
 import LinkButton from '../../ui/LinkButton';
 import Button from '../../ui/Button';
 import CartItem from '../cart/CartItem';
-import { useSelector } from 'react-redux';
-
-const fakeCart = [
-  {
-    pizzaId: 12,
-    name: 'Mediterranean',
-    quantity: 2,
-    unitPrice: 16,
-    totalPrice: 32,
-  },
-  {
-    pizzaId: 6,
-    name: 'Vegetale',
-    quantity: 1,
-    unitPrice: 13,
-    totalPrice: 13,
-  },
-  {
-    pizzaId: 11,
-    name: 'Spinach and Mushroom',
-    quantity: 1,
-    unitPrice: 15,
-    totalPrice: 15,
-  },
-];
+import { useDispatch, useSelector } from 'react-redux';
+import { clearCart } from './cartSlice';
 
 function Cart() {
+  const dispatch = useDispatch();
   const { username } = useSelector((store) => store.user);
-  const cart = fakeCart;
+  const { cart } = useSelector((store) => store.cart);
 
   return (
     <div className="px-4 py-3">
@@ -48,7 +26,9 @@ function Cart() {
         <Button to="/order/new" type="primary">
           Order pizzas
         </Button>
-        <Button type="secondary">Clear cart</Button>
+        <Button type="secondary" onClick={() => dispatch(clearCart())}>
+          Clear cart
+        </Button>
       </div>
     </div>
   );
