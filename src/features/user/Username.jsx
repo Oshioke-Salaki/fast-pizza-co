62f4c0dc9205ae2c59493f82d4aa6f08178970b6
Oshioke-Salaki <salaki1902@gmail.com>
2023-09-12 08:50:56 -0600

    update

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,7 +1,10 @@
+import { useSelector } from 'react-redux';
+
 function Username() {
-  return (
-    <p className="hidden text-sm font-semibold md:block">Oshioke Salaki</p>
-  );
+  const { username } = useSelector((store) => store.user);
+
+  if (!username) return null;
+  return <p className="hidden text-sm font-semibold md:block">{username}</p>;
 }
 
 export default Username;
